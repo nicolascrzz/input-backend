35b4f5d6e3ad1dcb3769510be402a91c321f5a13
Nicolas <nicolas.crzz14@gmail.com>
2025-12-30 18:25:07 -0300

    backend v1.0.3

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const XLSX = require("xlsx");
+const { Pool } = require("pg")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const { pool } = require("pg")
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
